0e44d03e27fff19761b3edc100f46ac1ac5f9f36
Kobi Hari <kobi2294@yahoo.com>
2019-02-17 10:54:15 +0200

    Added Design time resource dictionary support

---
+++ Source/MvvmKit/Tools/Exec.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MvvmKit
+{
+    public static class Exec
+    {
+        public static bool IsDesignTime
+        {
+            get
+            {
+                var designTimeEvaluator = new DependencyObject();
+
+                return DesignerProperties.GetIsInDesignMode(designTimeEvaluator);
+            }
+        }
+
+        public static bool IsRunTime
+        {
+            get
+            {
+                return !IsDesignTime;
+            }
+        }
+
+        public static bool IsOnUiThread()
+        {
+            var dispatcher = Dispatcher.FromThread(Thread.CurrentThread);
+
+            var res = (System.Windows.Application.Current != null) && 
+                (dispatcher == System.Windows.Application.Current.Dispatcher);
+
+            return res;
+        }
+
+        public static async Task RunOnUi(Action a)
+        {
+            await Application.Current.Dispatcher.InvokeAsync(a);
+        }
+    }
+}
---
+++ Source/MvvmKit/Ui/Controls/Design Time Support/SelectiveResources.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace MvvmKit
+{
+    /// <summary>
+    /// Use this class to combine run time and design time resources.
+    /// <see cref="DesignTimeResource"/> instances that would be added to the <see cref="ResourceDictionary.MergedDictionaries"/> 
+    /// property, will only be loaded at design time. You can use it for design time resources, such as design time data or resources
+    /// that are defined in a dynamically loaded assembly
+    /// </summary>
+    public class SelectiveResources : ResourceDictionary
+    {
+        private readonly DtCollection _noopMergedDictionaries = new DtCollection();
+
+        private class DtCollection : ObservableCollection<ResourceDictionary>
+        {
+            protected override void InsertItem(int index, ResourceDictionary item)
+            {
+                // if the type is DesignTimeResource - we only add it in design time, 
+                // otherwise, we always add it
+                if ((item is DesignTimeResource) && (Exec.IsRunTime)) return;
+
+                // otherwise, add it in any condition
+                base.InsertItem(index, item);
+            }
+        }
+
+        public SelectiveResources()
+        {
+            var fieldInfo = typeof(ResourceDictionary).GetField("_mergedDictionaries", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fieldInfo != null)
+            {
+                fieldInfo.SetValue(this, _noopMergedDictionaries);
+            }
+        }
+    }
+
+}
