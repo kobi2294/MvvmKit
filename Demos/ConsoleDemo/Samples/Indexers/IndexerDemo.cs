3202e97d4407a400b68992df981f2c6c6f70fdf9
Kobi Hari <kobi2294@yahoo.com>
2019-04-18 10:17:13 +0300

    Fixes and demo for indexers

---
+++ Demos/ConsoleDemo/Samples/Indexers/IndexerDemo.cs
@@ -0,0 +1,32 @@
+﻿using MvvmKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo.Samples.Indexers
+{
+    public class League
+    {
+        private Dictionary<string, int> _scores = new Dictionary<string, int>();
+
+        public IReadOnlyIndexer<string, int, int> Scores { get; }
+
+        public League()
+        {
+            Scores = MvvmKit.Indexers.ReadOnly(_scores)
+                                     .And((int i) => _scores.ElementAt(i).Value);
+        }
+    }
+
+    public static class IndexerDemo
+    {
+        public static void Run()
+        {
+            var league = new League();
+            var liverpoolScore = league.Scores["Liverpool"];
+            var firstTeamScore = league.Scores[0];
+        }
+    }
+}
