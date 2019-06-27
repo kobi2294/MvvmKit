60de59529d3b6917ee94cec2fc3424150224f15f
Yaniv Madav <yanivma@magal-s3.com>
2019-06-27 17:58:58 +0300

    test commit

--- Source/MvvmKit/Services/State/ServiceCollectionField.cs
+++ Source/MvvmKit/Services/State/ServiceCollectionField.cs
@@ -1,10 +1,7 @@
 ﻿using MvvmKit.CollectionChangeEvents;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MvvmKit
