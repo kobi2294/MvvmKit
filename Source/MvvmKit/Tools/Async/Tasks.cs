c9fd3211822da8b9a9ff626f069fb7ba912311b0
Kobi Hari <kobi2294@yahoo.com>
2019-08-12 14:24:47 +0300

    Added some util mextensions method: - ToObservableCollection for enumerables - WhenAll for enumerable of task of T

--- Source/MvvmKit/Tools/Async/Tasks.cs
+++ Source/MvvmKit/Tools/Async/Tasks.cs
@@ -19,6 +19,11 @@ namespace MvvmKit
             return Task.FromResult(value);
         }
 
+        public static Task<T[]> WhenAll<T>(this IEnumerable<Task<T>> tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
+
         public static DeferredTask<T> ToDeferredTask<T>(this T value)
         {
             return new DeferredTask<T>(value);
