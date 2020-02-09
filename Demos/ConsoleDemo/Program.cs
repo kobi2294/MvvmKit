94c58a079c15d8d0565dfadb5ae82a82238452db
Kobi Hari <kobi2294@yahoo.com>
2020-02-09 19:04:46 +0200

    Added Diff Algorithm, and DiffApplier

--- Demos/ConsoleDemo/Program.cs
+++ Demos/ConsoleDemo/Program.cs
@@ -24,7 +24,7 @@ namespace ConsoleDemo
 
         static void Main(string[] args)
         {
-            ConsoleDemo.Samples.Permute.Main.Run();
+            ConsoleDemo.Samples.Diff.Main.Run();
             Console.ReadLine();
         }
     }
