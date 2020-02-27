f37784da9ff4238b17de84dde3e4c5885f2a317f
Kobi Hari <kobi2294@yahoo.com>
2020-02-27 18:41:17 +0200

    Added extension methods for ImmutableList<T>: - Upsert - UpsertWhere

---
+++ Source/MvvmKit/Tools/Extensions/ImmutableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmKit
+{
+    public static class ImmutableExtensions
+    {
+        public static ImmutableList<T> UpsertWhere<T>(this ImmutableList<T> source, T item, Func<T, bool> predicate)
+        {
+            var index = source.IndexOf(predicate);
+            if (index < 0) return source.Add(item);
+
+            return source.SetItem(index, item);
+        }
+
+        public static ImmutableList<T> Upsert<T, K>(this ImmutableList<T> source, T item, Func<T, K> keySelector)
+        {
+            var key = keySelector(item);
+
+            return source.UpsertWhere(item, t => Equals(keySelector(t), key));
+        }
+
+        public static ImmutableList<T> Upsert<T>(this ImmutableList<T> source, T item)
+        {
+            return source.Upsert(item, t => t);
+        }
+    }
+}
