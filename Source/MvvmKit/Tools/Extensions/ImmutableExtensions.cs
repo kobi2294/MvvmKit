59b8437fb7c531e42f854fc302260cebf047a005
Kobi Hari <kobi2294@yahoo.com>
2020-02-27 19:04:36 +0200

    Some fixes to Upsert

--- Source/MvvmKit/Tools/Extensions/ImmutableExtensions.cs
+++ Source/MvvmKit/Tools/Extensions/ImmutableExtensions.cs
@@ -9,7 +9,7 @@ namespace MvvmKit
 {
     public static class ImmutableExtensions
     {
-        public static ImmutableList<T> UpsertWhere<T>(this ImmutableList<T> source, T item, Func<T, bool> predicate)
+        public static ImmutableList<T> UpsertWhere<T>(this ImmutableList<T> source, Func<T, bool> predicate, T item)
         {
             var index = source.IndexOf(predicate);
             if (index < 0) return source.Add(item);
@@ -17,19 +17,19 @@ namespace MvvmKit
             return source.SetItem(index, item);
         }
 
-        public static ImmutableList<T> Upsert<T, K>(this ImmutableList<T> source, T item, Func<T, K> keySelector)
+        public static ImmutableList<T> Upsert<T, K>(this ImmutableList<T> source, Func<T, K> keySelector, T item)
         {
             var key = keySelector(item);
 
-            return source.UpsertWhere(item, t => Equals(keySelector(t), key));
+            return source.UpsertWhere(t => Equals(keySelector(t), key), item);
         }
 
         public static ImmutableList<T> Upsert<T>(this ImmutableList<T> source, T item)
         {
-            return source.Upsert(item, t => t);
+            return source.Upsert(t => t, item);
         }
 
-        public static VersionedList<T> UpsertWhere<T>(this VersionedList<T> source, T item, Func<T, bool> predicate)
+        public static VersionedList<T> UpsertWhere<T>(this VersionedList<T> source, Func<T, bool> predicate, T item)
         {
             var index = source.IndexOf(predicate);
             if (index < 0) return source.Add(item);
@@ -37,16 +37,16 @@ namespace MvvmKit
             return source.SetItem(index, item);
         }
 
-        public static VersionedList<T> Upsert<T, K>(this VersionedList<T> source, T item, Func<T, K> keySelector)
+        public static VersionedList<T> Upsert<T, K>(this VersionedList<T> source, Func<T, K> keySelector, T item)
         {
             var key = keySelector(item);
 
-            return source.UpsertWhere(item, t => Equals(keySelector(t), key));
+            return source.UpsertWhere(t => Equals(keySelector(t), key), item);
         }
 
         public static VersionedList<T> Upsert<T>(this VersionedList<T> source, T item)
         {
-            return source.Upsert(item, t => t);
+            return source.Upsert(t => t, item);
         }
 
     }
