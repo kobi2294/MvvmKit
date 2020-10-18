8b24fad0bb5116df69b587ed7454e724baa7c950
Kobi Hari <kobi2294@yahoo.com>
2020-10-18 17:49:00 +0300

    Console window now runs on itw own thread

--- Source/MvvmKit/Ui/DevTools/ConsoleWindow.xaml.cs
+++ Source/MvvmKit/Ui/DevTools/ConsoleWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace MvvmKit
 {
@@ -27,10 +29,27 @@ namespace MvvmKit
 
         public static ConsoleWindow CreateAndShow()
         {
-            var win = new ConsoleWindow();
-            win.Show();
-            return win;
+            var tsc = new TaskCompletionSource<ConsoleWindow>();
+
+            Thread thread = new Thread(() =>
+            {
+                var win = new ConsoleWindow();
+                win.Show();
+
+                win.Closed += (s, e) => win.Dispatcher.InvokeShutdown();
+
+                tsc.SetResult(win);
+                Dispatcher.Run();
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            thread.Start();
+
+            return tsc.Task.Result;
+
         }
+
 
         public void Write(string text, Brush foreground = null, int length = 0)
         {
